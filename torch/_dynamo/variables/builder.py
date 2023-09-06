1a131db56e6a61f25caa5d24d3b77f953764d6f0
ydwu4 <ydwu2014@gmail.com>
2023-09-06 14:16:34 -0700

    Support SymBool input to torch.compile
    ghstack-source-id: 026f07826262219eb9bf060b2867faf43dd424c0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/107850

--- torch/_dynamo/variables/builder.py
+++ torch/_dynamo/variables/builder.py
@@ -45,6 +45,7 @@ from ..side_effects import SideEffects
 from ..source import (
     AttrSource,
     ConstantSource,
+    ConvertIntSource,
     GetItemSource,
     GlobalWeakRefSource,
     is_constant_source,
@@ -685,6 +686,45 @@ class VariableBuilder:
                 value,
                 source=self.source,
                 guards=make_guards(GuardBuilder.FUNCTION_MATCH),
+            )
+        elif isinstance(value, torch.SymBool):
+            # Note: the idea here is to re-use the infra we've built for SymInt by simulating the
+            # user provided SymBool with a SymInt in dynamo.
+
+            # Concretely,
+            # 1. We created a SymInt in dynamo's shape_env, whose source is constructed as ConvertIntSource(self.source).
+            # so that guards on the SymInts can be effectively applied on the original SymBool in user program.
+            # 2. We create a SymBool based on the SymInt in dynamo's ShapeEnv. Because the original user program
+            # depends on the value being a SymBool. This allows dynamo to interpret the user's program correctly.
+
+            new_source = ConvertIntSource(self.source)
+            new_symint = self.tx.output.shape_env.create_unspecified_symint_and_symbol(
+                value.__int__(),
+                new_source,
+                dynamic_dim=DimDynamic.STATIC,
+            )
+            new_symbool = new_symint == 1
+
+            sym_node_proxy = self.tx.output.root_tracer.create_graph_input(
+                re.sub(r"[^a-zA-Z0-9]+", "_", self.name),
+                type(new_symbool),
+                source=new_source,
+            )
+            sym_node_proxy.node.meta["grapharg"] = GraphArg(
+                new_source,
+                new_symbool,
+                False,
+                None,
+                is_tensor=False,
+                example_strong_ref=new_symbool,
+            )
+            self.tx.output.tracked_fakes.append(
+                TrackedFake(new_symbool, new_source, None)
+            )
+            return SymNodeVariable(
+                sym_node_proxy,
+                new_symbool,
+                source=new_source,
             )
         else:
             result = UserDefinedObjectVariable(
